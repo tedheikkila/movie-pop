b3d9c9041a3372945dec4ae4eebdf6c32090f25a
Zacharyjg <guldb016@umn.edu>
2021-06-24 13:39:52 -0500

    Finished API calls as well as error checking with an incorrect movie name

--- 3-locations/3-locations.js
+++ 3-locations/3-locations.js
@@ -1,19 +1,25 @@
-let url = "https://api.watchmode.com/v1/search/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&search_field=name&search_value=ted&types=movie";
+let movieTitle = ""
+
+let searchUrl = "https://api.watchmode.com/v1/search/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&search_field=name&search_value=lebowski&types=movie";
+let sourceUrl = "https://api.watchmode.com/v1/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
+let sourceNames = [];
 
 function searchApi() {
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
+    fetch(searchUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        try {
             let id = data.title_results[0].id;
             let titleUrl = "https://api.watchmode.com/v1/title/" + id + "/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
             titleSourcesApi(titleUrl);
+        } catch (error) {
+            console.log("Incorrect movie title")
+        }
     });
 }
-
-searchApi();
 
 function titleSourcesApi(titleIdUrl) {
     fetch(titleIdUrl)
@@ -24,9 +30,37 @@ function titleSourcesApi(titleIdUrl) {
         let locations = data;
         locations.sort((a, b) => (a.source_id > b.source_id) ? 1 : -1);
         console.log(locations);
+
+        let sourceIds = [];
+        for (let i of locations) {
+            if (!sourceIds.includes(i.source_id)) {
+                sourceIds.push(i.source_id)
+            }
+        }
+        arrayOfSourceNames(sourceIds);
+        console.log(sourceIds);
     });
 }
 
-function deleteDuplicateSources() {
-    
+function arrayOfSourceNames(ls) {
+    fetch(sourceUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        let sources = data;
+        for (let i of ls) {
+            let sourceName = sources.find(function(source, index) {
+                if (source.id == i) {
+                    return true;
+                }
+            });
+
+            sourceNames.push(sourceName.name);
+        }
+        console.log(data);
+        console.log(sourceNames);
+    });
 }
+
+searchApi();
