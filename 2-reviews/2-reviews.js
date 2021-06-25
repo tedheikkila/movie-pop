a47a2ea5f783606b79bd38d7d1ae970218a8099b
Josh Renaud <josh.renaud41@gmail.com>
2021-06-25 11:58:09 -0500

    added for loop in function to pull all 20 results from critics picks

--- 2-reviews/2-reviews.js
+++ 2-reviews/2-reviews.js
@@ -1,22 +1,15 @@
 
-//..var apiUrl = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=lebowski&api-key=2zOVysM67JvX3UMtLIhU2eAWk8V1i0m8'
+
 
 var button = document.getElementById('searchbtn');
 
 
-var api = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=';
+var api = 'https://api.nytimes.com/svc/movies/v2/reviews/picks.json?query=';
 
 var apiKey = '&api-key=2zOVysM67JvX3UMtLIhU2eAWk8V1i0m8';
 
-var apiUrl = api + "lebowski"+ apiKey;
+var apiUrl = api + apiKey;
 
-
-function getTitleInput(){
-            
-    var titleVal = document.getElementById("titleSearch").value;
-    
-    console.log(titleVal);
-}
 
 
 
@@ -31,11 +24,33 @@ function getApi() {
     .then(function (data) {
 
         console.log(data);
+        displayMovies(data)
 
       
     })
 }
 
-button.addEventListener("click", getTitleInput);
+
+
+function displayMovies(data) {
+    for (var i = 0; i < data.results.length; i++) {
+    const movie = data.results[i];
+    const moviesDiv = document.getElementById("movies");
+    const movieTitle = movie.display_title;
+    const heading = document.createElement("h1");
+    heading.innerHTML = movieTitle;
+    moviesDiv.appendChild(heading);
+    
+    const movieSummary = movie.summary_short;
+    const summary = document.createElement("h3");
+    summary.innerHTML = movieSummary;
+        moviesDiv.appendChild(summary);
+        
+        const movieImage = document.createElement("img");
+        movieImage.src = movie.multimedia.src;
+        moviesDiv.appendChild(movieImage);
+    
+    }
+}
 
 button.addEventListener("click", getApi);
