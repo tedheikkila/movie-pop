b59708f6c38e2e8b7cbd96c5beecfc4a14ed8290
Zacharyjg <guldb016@umn.edu>
2021-06-25 10:40:03 -0500

    Now have correct API calls to get a list of the sources for a given movie

--- 3-locations/3-locations.js
+++ 3-locations/3-locations.js
@@ -1,66 +1,73 @@
-let movieTitle = ""
+let movieTitle, searchUrl = "";
 
-let searchUrl = "https://api.watchmode.com/v1/search/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&search_field=name&search_value=lebowski&types=movie";
 let sourceUrl = "https://api.watchmode.com/v1/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
 let sourceNames = [];
 
 function searchApi() {
     fetch(searchUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        try {
-            let id = data.title_results[0].id;
-            let titleUrl = "https://api.watchmode.com/v1/title/" + id + "/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
-            titleSourcesApi(titleUrl);
-        } catch (error) {
-            console.log("Incorrect movie title")
-        }
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            try {
+                let id = data.title_results[0].id;
+                let titleUrl = "https://api.watchmode.com/v1/title/" + id + "/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
+                titleSourcesApi(titleUrl);
+            } catch (error) {
+                console.log("Incorrect movie title")
+            }
+        });
 }
 
 function titleSourcesApi(titleIdUrl) {
     fetch(titleIdUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        let locations = data;
-        locations.sort((a, b) => (a.source_id > b.source_id) ? 1 : -1);
-        console.log(locations);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let locations = data;
+            locations.sort((a, b) => (a.source_id > b.source_id) ? 1 : -1);
+            console.log(locations);
 
-        let sourceIds = [];
-        for (let i of locations) {
-            if (!sourceIds.includes(i.source_id)) {
-                sourceIds.push(i.source_id)
+            let sourceIds = [];
+            for (let i of locations) {
+                if (!sourceIds.includes(i.source_id)) {
+                    sourceIds.push(i.source_id)
+                }
             }
-        }
-        arrayOfSourceNames(sourceIds);
-        console.log(sourceIds);
-    });
+            arrayOfSourceNames(sourceIds);
+            console.log(sourceIds);
+        });
 }
 
 function arrayOfSourceNames(ls) {
     fetch(sourceUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        let sources = data;
-        for (let i of ls) {
-            let sourceName = sources.find(function(source, index) {
-                if (source.id == i) {
-                    return true;
-                }
-            });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let sources = data;
+            for (let i of ls) {
+                let sourceName = sources.find(function (source, index) {
+                    if (source.id == i) {
+                        return true;
+                    }
+                });
 
-            sourceNames.push(sourceName.name);
-        }
-        console.log(data);
-        console.log(sourceNames);
-    });
+                sourceNames.push(sourceName.name);
+            }
+            console.log(data);
+            console.log(sourceNames);
+        });
 }
 
+function movieTitleToSearch(title) {
+    title = title.trim();
+    movieTitle = title.replace(/ /g, "%20");
+    searchUrl = "https://api.watchmode.com/v1/search/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&search_field=name&search_value=" + movieTitle + "&types=movie";
+}
+
+movieTitleToSearch("john wick");
+console.log(searchUrl);
 searchApi();
