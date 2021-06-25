8fdc999907abdabaaed582619f464e6a1c4d8513
Ted Heikkila <ted.heikkila@gmail.com>
2021-06-25 11:58:44 -0500

    basic js added

--- 1-homepage/1-homepage.js
+++ 1-homepage/1-homepage.js
@@ -1,19 +1,41 @@
+// group one 1, movie pop, js for 1-homepage, 6-24-21
+
+// global vars
+const searchReviewButton = document.querySelector('#search-review-button');
+const searchTrailerButton = document.querySelector('#search-trailer-button')
+const searchLocationButton = document.querySelector('#search-location-button')
+let trailerInputEl = document.querySelector('#trailer-title');
+let locationInputEl = document.querySelector('#location-title');
 
 
-var searchReviewButton = document.querySelector('#search-review-button');
-var trailerInputEl = document.querySelector('#trailer-title');
-var locationInputEl = document.querySelector('#location-title');
 
+var formReviewHandler = function (event) {
+    console.log(event)
+}
 
 var formTrailerHandler = function (event) {
     event.preventDefault();
   
-    var review = reviewInputEl.value.trim();
+    var trailer = trailerInputEl.value.trim();
   
-    if (review) {
-      getUserRepos(review, trailer, location);
+    if (trailer) {
+      getTrailerApi(trailer);
   
-      reviewInputEl.value = '';
+      trailerInputEl.value = '';
+    } else {
+      alert('Please enter a movie title');
+    }
+  };
+
+  var formLocationHandler = function (event) {
+    event.preventDefault();
+  
+    var location = locationInputEl.value.trim();
+  
+    if (location) {
+      getLocation(location);
+  
+      locationInputEl.value = '';
     } else {
       alert('Please enter a movie title');
     }
@@ -32,12 +54,10 @@ var formTrailerHandler = function (event) {
 
 
 
+searchReviewButton.addEventListener('click', formReviewHandler);
+
+searchTrailerButton.addEventListener('click', formTrailerHandler);
+
+searchLocationButton.addEventListener('click', formLocationHandler)
 
 
-
-
-
-
-
-
-searchtrailerButton.addEventListener('click', formTrailerHandler);
