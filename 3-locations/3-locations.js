aadf7247667be77c3300f7693599f74cb853c05b
Zacharyjg <guldb016@umn.edu>
2021-06-28 09:23:30 -0500

    Fixed bugs and added case handling for when displaying previous searches

--- 3-locations/3-locations.js
+++ 3-locations/3-locations.js
@@ -1,13 +1,41 @@
-let movieTitle, searchUrl = "";
-
-let sourceUrl = "https://api.watchmode.com/v1/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
+// Setting up variables that are used later to store various bits of data
+let movieTitle = "";
 let sourceNames = [];
+let localStorageArr = [];
+
+// Selectors for various elements in the html
 let sourceListParent = document.getElementById("source-list");
 let searchButton = document.getElementById("search-button");
+let previousSearchesDiv = document.getElementById("previous-searched-movies");
 
+// All of the consts for the several API calls made throughout this webpage
+const sourceUrl = "https://api.watchmode.com/v1/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
 const searchUrlBeginning = "https://api.watchmode.com/v1/search/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&search_field=name&types=movie&search_value=";
+const titleUrlEnding = "/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
+const titleUrlBeginning = "https://api.watchmode.com/v1/title/";
 
-function searchApi() {
+
+
+function setMovieTitle(titleName) {
+    movieTitle = titleName;
+}
+
+function setPreviousSearch(titleName) {
+    let localStorageData = JSON.parse(localStorage.getItem("titles"));
+    if (localStorageData != null) {
+        if (!localStorageData.includes(titleName)) {
+            localStorageArr.push(titleName);
+            localStorage.setItem("titles", JSON.stringify(localStorageArr));
+        } else {
+            localStorage.setItem("titles", JSON.stringify(localStorageArr));
+        }
+    } else {
+        localStorageArr.push(titleName);
+        localStorage.setItem("titles", JSON.stringify(localStorageArr));
+    }
+}
+
+function searchApi(searchUrl) {
     fetch(searchUrl)
         .then(function (response) {
             return response.json();
@@ -16,13 +44,20 @@ function searchApi() {
             console.log(data);
             try {
                 if (data.title_results.length == 0) {
+                    // TODO: Add modal to say that the search cant find given movie name
                     console.log("Sorry but no movie came up with that name");
                 } else {
                     let id = data.title_results[0].id;
-                    let titleUrl = "https://api.watchmode.com/v1/title/" + id + "/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjDe&regions=US";
+                    setMovieTitle(data.title_results[0].name);
+                    setPreviousSearch(data.title_results[0].name);
+                    let searchedMovie = document.getElementById("searched-movie");
+                    searchedMovie.textContent = movieTitle + " can be watched from the following streaming services:"
+
+                    let titleUrl = titleUrlBeginning + id + titleUrlEnding;
                     titleSourcesApi(titleUrl);
                 }
             } catch (error) {
+                console.log(error);
                 console.log("Incorrect movie title")
             }
         });
@@ -36,8 +71,6 @@ function titleSourcesApi(titleIdUrl) {
         .then(function (data) {
             let locations = data;
             locations.sort((a, b) => (a.source_id > b.source_id) ? 1 : -1);
-            // console.log(locations);
-
             let sourceIds = [];
             for (let i of locations) {
                 if (!sourceIds.includes(i.source_id)) {
@@ -45,7 +78,6 @@ function titleSourcesApi(titleIdUrl) {
                 }
             }
             arrayOfSourceNames(sourceIds);
-            // console.log(sourceIds);
         });
 }
 
@@ -62,32 +94,28 @@ function arrayOfSourceNames(ls) {
                         return true;
                     }
                 });
-
                 sourceNames.push(sourceName.name);
             }
-            // console.log(data);
-            // console.log(sourceNames);
             displaySources(sourceNames);
-            
+            displayPreviousSearches();
         });
 }
 
 function movieTitleToSearch(title) {
-    // if(title.includes(":")) {
-
-    // }
-    let searchedMovie = document.getElementById("searched-movie");
-    title = title.trim();
-    searchedMovie.textContent = title + " can be watched from the following streaming services:"
-    movieTitle = title.replace(/ /g, "%20");
-    searchUrl = searchUrlBeginning + movieTitle;
-    searchApi();
+    if (title == "") {
+        // TODO: Add modal to say that the search cant handle certain punctuation title.includes(":")
+        console.log("Search can't handle these conditions");
+    } else {
+        title = title.trim();
+        movieTitle = title.replace(/ /g, "%20");
+        let searchUrl = searchUrlBeginning + movieTitle;
+        searchApi(searchUrl);
+    }
 }
 
 function displaySources(ls) {
     sourceListParent.innerHTML = "";
     for (let i of ls) {
-        console.log(i);
         let newSource = document.createElement("li");
         newSource.textContent = i;
         newSource.classList.add("body-text");
@@ -95,11 +123,41 @@ function displaySources(ls) {
     }
 }
 
-searchButton.addEventListener("click", function(event) {
+function displayPreviousSearches() {
+    previousSearchesDiv.innerHTML = "";
+    let localStorageData = JSON.parse(localStorage.getItem("titles"));
+    if (localStorageData != null) {
+        localStorageArr = localStorageData;
+
+        for (let i = 0; i < localStorageData.length; i++) {
+            let previousSearchCard = document.createElement("h3");
+            previousSearchCard.textContent = localStorageData[i];
+            previousSearchCard.classList.add("card");
+            previousSearchCard.classList.add("previous-movie");
+            previousSearchesDiv.appendChild(previousSearchCard);
+        }
+    }
+}
+
+
+
+// Below are the event listeners for the locations webapge
+// This event listener is for the submit button. On click it will call movieTitleToSearch() on whatever the given movie title is
+searchButton.addEventListener("click", function (event) {
     event.preventDefault();
     let searchField = document.getElementById("movie-search");
-    console.log(searchField.value);
     sourceNames = [];
     movieTitleToSearch(searchField.value);
     searchField.value = "";
+    displayPreviousSearches();
 });
+
+previousSearchesDiv.addEventListener("click", function (event) {
+    event.preventDefault();
+    let titleName = event.target.textContent;
+    sourceNames = [];
+    movieTitleToSearch(titleName);
+    displayPreviousSearches();
+});
+
+displayPreviousSearches();
