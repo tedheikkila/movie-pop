fcfa3cc9f9abaa47ecb7916c4e1d79c0031dfe00
Zacharyjg <guldb016@umn.edu>
2021-06-29 11:02:54 -0500

    Commented javascript code

--- 3-locations/3-locations.js
+++ 3-locations/3-locations.js
@@ -17,11 +17,13 @@ const titleUrlEnding = "/sources/?apiKey=nWQ1K1yKiv353tqprKAXswej5MlSUEmSSLqaDjD
 const titleUrlBeginning = "https://api.watchmode.com/v1/title/";
 
 
-
+// These are the setters for the Where to Watch webpage
+// This function simply sets the current movie title
 function setMovieTitle(titleName) {
     movieTitle = titleName;
 }
 
+// This function takes in the previous movie search and resets localStorage to include the newly searched movie
 function setPreviousSearch(titleName) {
     let localStorageData = JSON.parse(localStorage.getItem("titles"));
     if (localStorageData != null) {
@@ -37,30 +39,37 @@ function setPreviousSearch(titleName) {
     }
 }
 
+// This is the API call that searches for a specific movie title and returns data on that movie if found. The main thing taken from this API call is the movie ID
 function searchApi(searchUrl) {
     fetch(searchUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            // We have a try catch block here for the case where the searchUrl is incorrect for some reason and then we try to grab data from an undefined object. 
+            // try-catch block more so for develeopment purposes here
             try {
+                // One of the special cases that is handled for when you enter in a movie title and no movie comes back. Otherwise, go about as normal
                 if (data.title_results.length == 0) {
+                    sourceListParent.innerHTML = "";
                     searchedMovie.textContent = "Sorry, no movie came up with that name. Please make sure that the movie title is exactly correct in the search bar";
                 } else {
                     let id = data.title_results[0].id;
                     setMovieTitle(data.title_results[0].name);
                     setPreviousSearch(data.title_results[0].name);
-                    searchedMovie.textContent = movieTitle + " can be watched from the following streaming services:"
+                    searchedMovie.textContent = movieTitle + " can be watched from the following streaming services:";
                     let titleUrl = titleUrlBeginning + id + titleUrlEnding;
                     titleSourcesApi(titleUrl);
                 }
             } catch (error) {
+                sourceListParent.innerHTML = "";
                 console.log(error);
-                searchedMovie.textContent = "Error caught in the program. Please check the dev console for an error message"
+                searchedMovie.textContent = "Error caught in the program. Please check the dev console for an error message";
             }
         });
 }
 
+// This is the second API call which is a call to retrieve the sources for a given movie that is found in the search API
 function titleSourcesApi(titleIdUrl) {
     fetch(titleIdUrl)
         .then(function (response) {
@@ -68,17 +77,19 @@ function titleSourcesApi(titleIdUrl) {
         })
         .then(function (data) {
             let locations = data;
+            // This is a fancy little sort function that basically goes through and sorts all of the source IDs in ascending order to make it a little easier to read through
             locations.sort((a, b) => (a.source_id > b.source_id) ? 1 : -1);
             let sourceIds = [];
             for (let i of locations) {
                 if (!sourceIds.includes(i.source_id)) {
-                    sourceIds.push(i.source_id)
+                    sourceIds.push(i.source_id);
                 }
             }
             arrayOfSourceNames(sourceIds);
         });
 }
 
+// This is the third and final API call. This takes the list of sources for a given movie and goes and grabs the names for those sources and makes an array of the names to display
 function arrayOfSourceNames(ls) {
     fetch(sourceUrl)
         .then(function (response) {
@@ -99,8 +110,10 @@ function arrayOfSourceNames(ls) {
         });
 }
 
+// This function grabs the user inputted movie and does some case handling to make sure it is valid input and prepares it to be sent into the API call
 function movieTitleToSearch(title) {
-    if (title == "") {
+    if (title.trim() == "") {
+        sourceListParent.innerHTML = "";
         searchedMovie.textContent = "Please enter in an actual movie title"
     } else {
         title = title.trim();
@@ -110,6 +123,7 @@ function movieTitleToSearch(title) {
     }
 }
 
+// This function takes the list of source names and displays them in the HTML
 function displaySources(ls) {
     sourceListParent.innerHTML = "";
     if (ls.length == 0) {
@@ -127,6 +141,7 @@ function displaySources(ls) {
     }
 }
 
+// This function grabs all of the previous searches from localStorage and displays them on the screen
 function displayPreviousSearches() {
     previousSearchesDiv.innerHTML = "";
     let localStorageData = JSON.parse(localStorage.getItem("titles"));
@@ -154,6 +169,7 @@ searchButton.addEventListener("click", function (event) {
     displayPreviousSearches();
 });
 
+// This event listener is for when the user clicks on one of the previous searched movies and it will display that movies sources
 previousSearchesDiv.addEventListener("click", function (event) {
     event.preventDefault();
     let titleName = event.target.textContent;
@@ -162,10 +178,13 @@ previousSearchesDiv.addEventListener("click", function (event) {
     displayPreviousSearches();
 });
 
-clearButton.addEventListener("click", function(event) {
+// This event listener is for the clear button on previous searches. When clicked, it will clear all of the previous searches from the page
+clearButton.addEventListener("click", function (event) {
     event.preventDefault();
+    localStorageArr = [];
     localStorage.clear();
     displayPreviousSearches();
 });
 
+// This is called right when the page is loaded so any previous searches will be there on the webpage
 displayPreviousSearches();
